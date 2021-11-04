6018aed9bfbe7b76a40e12b84589d12a8ef30c68
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-11-04 15:56:12 +0900

    layout

---
+++ components/layout.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Layout.module.css';
+
+interface Props {
+	children: JSX.Element | JSX.Element[],
+}
+
+export default function Layout({ children }: Props) {
+	return <div className={styles.container}>
+		{children}
+	</div>
+}
