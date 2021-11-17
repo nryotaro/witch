1c2a7c5b3358cc2ea66448c40ba1d3315449803c
Nakamura, Ryotaro <ryotaro@T490.T490.local>
2021-11-17 11:59:29 +0900

    drop

--- pages/api/profiles.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { UserProfile } from '../../libs/profile';
-
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<UserProfile>) {
-  if (req.method == 'POST') {
-    res.status(201).json(req.body);
-  }
-}
